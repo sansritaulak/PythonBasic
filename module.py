a21b6e795606e4e6ce03461d5bec2d5422955690
sansritaulak <usansrita@gmail.com>
2022-07-10 18:08:03 +0545

    modules in python

---
+++ module.py
@@ -0,0 +1,13 @@
+import math
+print(math.sqrt(4))
+print(dir(math))
+
+import calendar
+cal = calendar.month(2016,1)
+print(cal)
+dir(calendar)
+
+#import your own module
+import IfStatement 
+print(per)
+
