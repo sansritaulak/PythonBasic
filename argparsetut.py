152526a27b400af84ba2043b8facd7a78ecc7cd3
sansritaulak <usansrita@gmail.com>
2022-07-18 22:56:07 +0545

    arg parse in python

---
+++ argparsetut.py
@@ -0,0 +1,52 @@
+#Program to input two numbers and a operation to be used and return the result of operation based on inputs
+#Posional argument 
+# import argparse
+# if __name__== "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("number1", help ="first number")
+#     parser.add_argument("number2", help ="second number")
+#     parser.add_argument("operation", help ="operation")
+
+#     args = parser.parse_args()
+#     print(args.number1)
+#     print(args.number2)
+#     print(args.operation)
+
+#     n1 = int(args.number1)
+#     n2 = int(args.number2)
+#     result = None
+    
+#     if args.operation == "add":
+#         result = n1+n2
+#     elif args.operation == "subtract":
+#         result = n1-n2
+#     elif args.operation == "multiply":
+#         result = n1*n2
+
+#     print(result)
+
+#for optional argument add --to arguments  
+import argparse
+if __name__== "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--number1", help ="first number")
+    parser.add_argument("--number2", help ="second number")
+    parser.add_argument("--operation", help ="operation", choices = ["add","subtract","multiply"])
+
+    args = parser.parse_args()
+    print(args.number1)
+    print(args.number2)
+    print(args.operation)
+
+    n1 = int(args.number1)
+    n2 = int(args.number2)
+    result = None
+    
+    if args.operation == "add":
+        result = n1+n2
+    elif args.operation == "subtract":
+        result = n1-n2
+    elif args.operation == "multiply":
+        result = n1*n2
+
+    print("Result : ", result)
